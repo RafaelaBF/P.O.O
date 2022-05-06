ad5a0954016813caa0e168c00b42b5994b8636a2
Rafaela Bezerra de Figueiredo <82965842+RafaelaBF@users.noreply.github.com>
2022-05-06 18:00:59 -0300

    Add arquivos aula2

---
+++ Aula2/Aula2/src/Program/Cao.java
@@ -0,0 +1,12 @@
+package Program;
+
+public class Cao { // Objeto cão sem contrutor
+	// Variáveis de intância da classe Cão
+	float tamanho;  
+	String raca;  
+	String nome;
+	
+	public void latir() { // Metodo da class Cao
+		System.out.println(nome + ": AuAuAuAuAu");
+	}
+}
