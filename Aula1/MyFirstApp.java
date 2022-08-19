664759a378b29c2a8971b9b1b319b8f829b464b2
Rafaela Bezerra de Figueiredo <82965842+RafaelaBF@users.noreply.github.com>
2022-08-19 08:46:13 -0300

    Delete Aula1 directory

--- Aula1/MyFirstApp.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstApp {
-	public static void main(String[] args){
-		System.out.println("qualquer coisa");
-		System.out.println("hueeeba " + args[0] + args[1] + ", bão?");
-	}
-}
--- Aula1/MySecondApp.java
+++
@@ -1,5 +0,0 @@
-public class MySecondApp {
-	public static void main(String[] args){
-		System.out.println("A soma é: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
-	}
-}
