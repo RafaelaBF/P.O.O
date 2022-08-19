94af1be41466873558b76ac8f4da2d9950ca4373
Rafaela Bezerra de Figueiredo <82965842+RafaelaBF@users.noreply.github.com>
2022-08-19 09:20:11 -0300

    Delete Aula8 directory

--- Aula8/Aula8/src/supper/Main.java
+++
@@ -1,9 +0,0 @@
-package supper;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		Leao leao = new Leao("Lion", "Leão", 23);
-		System.out.println("Nome do leão: " + leao.getNome());
-	}
-}
