da759451e506eef3576c73b647c320a7bd33dada
Rafaela Bezerra de Figueiredo <82965842+RafaelaBF@users.noreply.github.com>
2022-08-27 07:19:24 +1030

    Add files via upload

---
+++ Aula12/Aula12/src/metodoTrim/Principal.java
@@ -0,0 +1,12 @@
+package metodoTrim;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		String n = "      as bolas";
+		System.out.println(n);
+		n = n.trim();
+		System.out.println(n);
+	}
+
+}
